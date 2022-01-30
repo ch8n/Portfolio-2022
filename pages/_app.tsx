158272446a9ecfd37e64342d769da751e9d3258a
ch8n <chetan.garg36@gmail.com>
2022-01-31 00:15:25 +0530

    updated styles for resume, highlights, contact,connect,workexp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { AppProps } from 'next/app'
-
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import '../styles/index.css'
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return  <Component {...pageProps} />
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
-import { ReactNode } from "react";
+import { Children, ReactNode } from "react";
 import Layout from "../components/Layout";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+  faReddit,
+  faMedium,
+} from "@fortawesome/free-brands-svg-icons";
 
 type Props = {
   children: ReactNode;
@@ -9,7 +17,7 @@ type Props = {
 };
 
 const AboutMessage = () => (
-  <div className="mt-8">
+  <div className="mt-8 px-8 text-lg">
     <p>
       I’m a senior software Engineer from India. Currently working at
       Housing.com. Having 4+ Years of experince in Android development with
@@ -21,6 +29,159 @@ const AboutMessage = () => (
   </div>
 );
 
+const Resume = () => (
+  <div className="my-8">
+    <p className="text-3xl">Resume 🔖</p>
+    <p className="px-8 py-4 text-lg">
+      Download my resume from{" "}
+      <Link href="https://docs.google.com/document/d/1BqqJ05CoB9_neN3j5wR5XCcQOXIAdf1y9JGl9y-sD_Y/edit?usp=sharing">
+        <a target="_blank" className="text-teal-300">
+          here
+        </a>
+      </Link>
+    </p>
+  </div>
+);
+
+const Highlights = () => (
+  <div className="my-8">
+    <p className="text-3xl">Highlights 🔦</p>
+    <ul className="list-disc px-8 py-4 text-lg">
+      <li>
+        Senior Android Developer at housing.com, building digital realestate
+        listing application for Proptiger.
+      </li>
+      <li>
+        Senior Android Developer at Dutshe Telekom Digital Lab, building OTT
+        platform.
+      </li>
+      <li>
+        Core team member at Kotlin Mumbai, Organize and Host Kotlin & Android
+        talks.
+      </li>
+      <li>
+        Core team member at the Code Monk, contribute and develop Open Source
+        Project in Android & Kotlin.
+      </li>
+      <li>
+        Feature at Various newsletter and magazine, KotlinWeekly, AndroidWeekly,
+        ProAndroidDev, DroidCon
+      </li>
+    </ul>
+  </div>
+);
+
+const Contact = () => (
+  <div className="my-8">
+    <p className="text-3xl">Contact 📞</p>
+    <p className="px-8 py-4 text-lg">
+      You can connect with me through{" "}
+      <Link href="https://t.me/ch810">
+        <a target="_blank" className="text-teal-300">
+          Telegram @ChetanGupta
+        </a>
+      </Link>{" "}
+      or Write me at{" "}
+      <Link href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJvmZfwGcvVTPnBJSgvfWcRGMFwznXdfkwkXHRCfSTrzRcmlpnZJcFcbkzZsxCmfTNRZBtg">
+        <a target="_blank" className="text-teal-300">
+          dev.ch8n@gmail.com
+        </a>
+      </Link>
+    </p>
+  </div>
+);
+
+const SocialIcon = ({ icon, label, url }) => (
+  <a href={url} target="_blank">
+    <div className="px-2 text-center">
+      <FontAwesomeIcon icon={icon} style={{ fontSize: 36, color: "white" }} />
+      <p>{label}</p>
+    </div>
+  </a>
+);
+
+const Connect = () => (
+  <div className="my-8">
+    <p className="text-3xl">Connect 🌎</p>
+    <p className="px-8 py-4 text-lg">You can follow me on social media on :</p>
+    <div className="flex flex-row">
+      <SocialIcon
+        icon={faTwitter}
+        label={"Twitter"}
+        url={"https://bit.ly/ch8n-twitter"}
+      />
+      <SocialIcon
+        icon={faLinkedin}
+        label={"LinkedIn"}
+        url={"https://bit.ly/ch8n-linkdIn"}
+      />
+      <SocialIcon
+        icon={faGithub}
+        label={"Github"}
+        url={"https://github.com/ch8n"}
+      />
+      <SocialIcon
+        icon={faReddit}
+        label={"Reddit"}
+        url={"https://www.reddit.com/user/dev-ch8n"}
+      />
+      <SocialIcon
+        icon={faMedium}
+        label={"Medium"}
+        url={"https://bit.ly/ch8n-medium-blog"}
+      />
+    </div>
+  </div>
+);
+
+const WorkExperience = () => (
+  <div className="my-8">
+    <p className="text-3xl">Work Experience ☕️</p>
+    <ul className="px-8 py-4 text-lg list-none">
+      <li>
+        Read more details about my past jobs from{" "}
+        <Link href="https://chetangupta.net/experience/">
+          <a target="_blank" className="text-teal-300">
+            here
+          </a>
+        </Link>
+        ,
+      </li>
+      <li>
+        {" "}
+        if your interested to look into my community contributions click{" "}
+        <Link href="https://chetangupta.net/experience/">
+          <a target="_blank" className="text-teal-300">
+            here
+          </a>
+        </Link>{" "}
+        and
+      </li>
+      <li>
+        {" "}
+        If you want to see my personal projects click{" "}
+        <Link href="https://chetangupta.net/experience/">
+          <a target="_blank" className="text-teal-300">
+            here
+          </a>
+        </Link>
+        .
+      </li>
+      <li>
+        {" "}
+        If you want to see where I have been featured, visit my indiviual Post.
+        It has feature section on it. For more connect with me via{" "}
+        <Link href="https://bit.ly/ch8n-linkdIn">
+          <a target="_blank" className="text-teal-300">
+            LinkedIn
+          </a>
+        </Link>{" "}
+        for more.
+      </li>
+    </ul>
+  </div>
+);
+
 const AboutContent = ({ title, children }: Props) => (
   <div className="outline-dashed my-8 outline-4  outline-red-500">
     <p className="text-3xl">{title}</p>
@@ -29,46 +190,12 @@ const AboutContent = ({ title, children }: Props) => (
 );
 
 const AboutDetails = () => (
-  <div className="about-details">
-    <AboutContent title="Resume 🔖">
-      <p>Download my resume from here</p>
-    </AboutContent>
-
-    <AboutContent title="Highlights 🔦">
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem velit,
-        nemo aperiam quidem, laudantium inventore assumenda architecto neque
-        ratione dolor quod explicabo in optio natus autem earum voluptatibus.
-        Asperiores, animi.
-      </p>
-    </AboutContent>
-
-    <AboutContent title="Contact  📞">
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem velit,
-        nemo aperiam quidem, laudantium inventore assumenda architecto neque
-        ratione dolor quod explicabo in optio natus autem earum voluptatibus.
-        Asperiores, animi.
-      </p>
-    </AboutContent>
-
-    <AboutContent title="Connect  🌎">
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem velit,
-        nemo aperiam quidem, laudantium inventore assumenda architecto neque
-        ratione dolor quod explicabo in optio natus autem earum voluptatibus.
-        Asperiores, animi.
-      </p>
-    </AboutContent>
-
-    <AboutContent title="Work Experience  ☕️">
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem velit,
-        nemo aperiam quidem, laudantium inventore assumenda architecto neque
-        ratione dolor quod explicabo in optio natus autem earum voluptatibus.
-        Asperiores, animi.
-      </p>
-    </AboutContent>
+  <div className="about-details px-8">
+    <Resume />
+    <Highlights />
+    <Contact />
+    <Connect />
+    <WorkExperience />
 
     <AboutContent title="Qualification 🎓">
       <p>
@@ -109,7 +236,7 @@ const AboutDetails = () => (
 );
 
 const ContentIndex = () => (
-  <div className="fixed top-0 right-0 w-36 outline-dashed my-8 outline-4  outline-red-500">
+  <div className="hidden top-0 right-0 w-36 outline-dashed my-8 outline-4  outline-red-500">
     <ul className="list-none hover:list-disc">
       <p className="text-2xl">Content 📒</p>
       <li>Highlights</li>
@@ -125,9 +252,9 @@ const ContentIndex = () => (
 );
 
 const BottomNavigation = () => (
-  <div className="bottom-navigation">
+  <div className="bottom-navigation ">
     {/* bottom-navigation */}
-    <div className="fixed bottom-0 grid gap-auto grid-cols-5 grid-rows-1">
+    <div className="fixed bg-stone-800 bottom-0 grid gap-auto grid-cols-5 grid-rows-1">
       <div className="outline-dashed outline-4  outline-red-500">
         {/* browse */}
         Home
@@ -154,7 +281,7 @@ const BottomNavigation = () => (
 
 const AboutPage = () => (
   <div className="outline-dashed outline-4 outline-red-500 container">
-    <div className="mt-8">
+    <div className="mt-8 px-8">
       <Image
         src="/images/intro-about.png"
         alt="Picture of the author"
